a156dd589b468cfa64fe00f0a07868f8ae922574
ParagPat20 <parag.pat20@gmail.com>
2024-11-05 19:15:20 +0530

    NewChanges

--- app.py
+++ app.py
@@ -1,42 +1,35 @@
-# app.py
 import os
 import threading
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 import webview
 
-# Define the directory where HTML and static files are located
 web_dir = os.path.join(os.path.dirname(__file__), 'static')
-os.chdir(web_dir)  # Set working directory to /static
-
+os.chdir(web_dir)  
 class CustomHandler(SimpleHTTPRequestHandler):
     def do_POST(self):
         if self.path == '/shutdown':
-            # Shutdown WebView and HTTP server
             if webview_window:
                 webview_window.destroy()
             self.send_response(200)
             self.end_headers()
             self.wfile.write(b"Server shutting down...")
             print("Exiting Processes")
-            os._exit(0)  # Terminate the entire Python process
+            os._exit(0) 
 
-# Start HTTP server in a separate thread
 def start_http_server():
     global httpd
     httpd = HTTPServer(("127.0.0.1", 5000), CustomHandler)
     httpd.serve_forever()
 
-# Function to stop the server
 def stop_server():
     if httpd:
         httpd.shutdown()
 
-# Function to create a web view window
 def create_window():
     global webview_window
     webview_window = webview.create_window(
         "Standalone HTTP Server Application",
-        "http://127.0.0.1:5000/index.html",  # Point directly to index.html in /static
+        "http://127.0.0.1:5000/index.html", 
         width=1600,
         height=900,
         frameless=True
@@ -44,9 +37,7 @@ def create_window():
     webview.start()
 
 if __name__ == "__main__":
-    # Start HTTP server in a separate thread
     http_thread = threading.Thread(target=start_http_server)
     http_thread.start()
 
-    # Create and run the web view window
     create_window()
