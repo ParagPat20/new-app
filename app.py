d84c133b2be261fa1fab65810d3f789d85d9a5f1
ParagPat20 <parag.pat20@gmail.com>
2024-11-06 12:12:07 +0530

    ada

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import threading
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 import subprocess
 import time
+import platform
 
 # Define the directory for the static files
 web_dir = os.path.join(os.path.dirname(__file__), 'static')
@@ -27,13 +28,22 @@ def start_http_server():
 def start_electron_app():
     # Wait a few seconds for the HTTP server to be ready
     time.sleep(1)
-    
+
+    # Check the operating system
+    if platform.system() == "Windows":
+        # For Windows, use the full path to the Electron executable
+        electron_executable = r'C:\Users\LOQ\AppData\Roaming\npm\node_modules\electron\dist\electron.exe'
+    elif platform.system() == "Linux" and "raspberrypi" in platform.uname().machine.lower():
+        # For Raspberry Pi (Linux), just use 'electron' (assuming it's installed globally)
+        electron_executable = 'electron'
+    else:
+        raise EnvironmentError("Unsupported operating system")
+
     # Launch Electron app with the disable-gpu flag and other relevant flags
-    electron_process = subprocess.Popen(['electron', '--disable-gpu', '--no-sandbox', '--use-gl=swiftshader', 'main.js'])
-    
+    electron_process = subprocess.Popen([electron_executable, '--disable-gpu', '--no-sandbox', '--use-gl=swiftshader', 'main.js'])
+
     # Wait until Electron app is closed
     electron_process.communicate()
-
 
 if __name__ == "__main__":
     # Start the HTTP server in a separate thread
