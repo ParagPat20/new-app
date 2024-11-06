25a82625a9abd793e953e8f1c5da29329719e5d7
ParagPat20 <parag.pat20@gmail.com>
2024-11-06 12:51:18 +0530

    da

--- keyboardstart.sh
+++ keyboardstart.sh
@@ -6,6 +6,6 @@ then
     echo "wvkbd-mobintl is already running."
 else
     # Start wvkbd-mobintl with the desired options
-    wvkbd-mobintl -L 300 -bg 90EE90 --press ff00f0 --press-sp ff00f0 -O &
+    wvkbd-mobintl -L 300 -bg 90EE90 --press 00ff00 --press-sp 00ff00 -O &
     echo "wvkbd-mobintl started."
 fi
